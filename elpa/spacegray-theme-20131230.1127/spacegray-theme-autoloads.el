1830cf90a5a6fa19d6e5819dc7c3cd7beb9ab46d
Andrey Neverov <neverov.and@gmail.com>
2014-10-28 19:01:07 +0300

    Removed unused themes

--- elpa/spacegray-theme-20131230.1127/spacegray-theme-autoloads.el
+++
@@ -1,26 +0,0 @@
-;;; spacegray-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "spacegray-theme" "spacegray-theme.el" (21582
-;;;;;;  30825 0 0))
-;;; Generated autoloads from spacegray-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("spacegray-theme-pkg.el") (21582 30825
-;;;;;;  410666 0))
-
-;;;***
-
-(provide 'spacegray-theme-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; spacegray-theme-autoloads.el ends here
