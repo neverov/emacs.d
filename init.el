6b4071e6edb55899d2087ebb55de129dabc95745
Andrey Neverov <aneverov@Andreys-MacBook-Air.local>
2015-01-14 09:05:29 -0800

    Updated packages

--- init.el
+++ init.el
@@ -48,11 +48,6 @@
 ;; use spaces instead of tabs
 (setq-default indent-tabs-mode nil)
 
-;; neotree
-(add-to-list 'load-path "~/.emacs.d/neotree")
-(require 'neotree)
-(global-set-key [f8] 'neotree-toggle)
-
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook                  #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
@@ -142,11 +137,13 @@
  '(jsx-indent-level 2)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
- '(visible-bell nil))
+ '(visible-bell nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#1d1d1d" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Ubuntu Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#1d1d1d" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 160 :width normal :foundry "nil" :family "Fira Mono")))))
